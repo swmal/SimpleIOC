5d970358fabaa35ba740ae08e99e9b5e56460aec
Mats Alm <mats.alms.epost@gmail.com>
2012-01-22 00:35:49 +0100

    Added ExpressionActivator

---
+++ src/SimpleIOC.Core/Activation/ExpressionActivator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace SimpleIOC.Core.Activation
+{
+    public class ExpressionActivator : ITypeActivator
+    {
+        private readonly IConstructorSelector _constructorSelector;
+        private readonly Container _container;
+        private Dictionary<Type, Delegate> _lambdas = new Dictionary<Type, Delegate>();
+
+        public ExpressionActivator(Container container, IConstructorSelector constructorSelector)
+        {
+            _constructorSelector = constructorSelector;
+            _container = container;
+        }
+
+        public object CreateInstance(Type type)
+        {
+            if (!_lambdas.ContainsKey(type))
+            {
+                var constructorInfo = _constructorSelector.Select(type, _container);
+                var arguments = GetArguments(constructorInfo).ToArray();
+                var ctorCall = Expression.New(constructorInfo, arguments);
+                var lambda = Expression.Lambda(ctorCall, Enumerable.Empty<ParameterExpression>());
+                _lambdas.Add(type, lambda.Compile());
+            }
+            return _lambdas[type].DynamicInvoke();
+        }
+
+        private IEnumerable<Expression> GetArguments(ConstructorInfo constructorInfo)
+        {
+            foreach (var info in constructorInfo.GetParameters())
+            {
+                var type = info.ParameterType;
+                var item = _container.Resolve(type);
+                yield return Expression.Constant(item);
+            }
+        }
+    }
+}
---
+++ src/SimpleIOC.Tests/Activation/ExpressionActivatorTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleIOC.Core;
+using SimpleIOC.Tests.TestItems;
+using SimpleIOC.Core.Activation;
+
+namespace SimpleIOC.Tests.Activation
+{
+    [TestClass]
+    public class ExpressionActivatorTests
+    {
+        [TestMethod]
+        public void CreateInstance()
+        {
+            var containerBuilder = ContainerBuilder.CreateBuilder();
+            containerBuilder.Register<ITestInterface2>().ImplementedBy<TestInterface2Impl>();
+            var container = containerBuilder.Build();
+
+            var activator = new ExpressionActivator((Container)container, new ConstructorSelector());
+            var result = activator.CreateInstance(typeof(TestInterface2Impl));
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void ShouldBuildAnInstanceWithGreedyConstructor()
+        {
+            var containerBuilder = ContainerBuilder.CreateBuilder();
+            containerBuilder.Register<ITestInterface1>().ImplementedBy<TestInterfaceImpl>();
+            containerBuilder.Register<ITestInterface2>().ImplementedBy<TestInterface2Impl>();
+            containerBuilder.Register<ITestInterface3>().ImplementedBy<TestInterface3Impl>();
+            var container = containerBuilder.Build();
+
+            var activator = new ExpressionActivator((Container)container, new ConstructorSelector());
+            var result = activator.CreateInstance(typeof(TestInterfaceImpl));
+            Assert.IsNotNull(result);
+        }
+    }
+}
